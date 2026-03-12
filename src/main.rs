f14c10d2d58c7e379f278aeb6f43789032c0a8b7
c4ff0e <76472853+c4ff0e@users.noreply.github.com>
2026-03-12 23:45:37 +0200

    com port finding

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,13 @@ use crate::metrics::cpu_ram;
 use crate::metrics::gpu;
 
 use crate::usb::serialize;
+use crate::usb::send;
 fn main() {
     let mut cpu_ram_metrics = cpu_ram::CpuRamMetrics::new();
     let mut gpu_metrics = gpu::GpuMetrics::new();
     loop {
         cpu_ram_metrics.refresh();
         gpu_metrics.refresh();
-        println!("================================================");
-        println!("CPU Usage: {:.2}%", cpu_ram_metrics.cpu_usage);
-        println!("CPU Name: {}", cpu_ram_metrics.cpu_name);
-        println!("CPU Frequency: {} MHz", cpu_ram_metrics.cpu_frequency);
-        println!("Total RAM: {} MB", cpu_ram_metrics.total_ram / 1024 / 1024);
-        println!("Used RAM: {} MB", cpu_ram_metrics.used_ram / 1024 / 1024);
-        println!("Is Supported: {}", cpu_ram_metrics.is_supported);
-        println!("================================================");
-        println!("GPU: {}", gpu_metrics.gpu_name);
-        println!("GPU Usage: {:.2}%", gpu_metrics.gpu_usage);
-        println!("GPU Temp: {}°C", gpu_metrics.gpu_temp);
-        println!("GPU Memory Total: {} MB", gpu_metrics.gpu_memory_total / 1024 / 1024);
-        println!("GPU Memory Used: {} MB", gpu_metrics.gpu_memory_used / 1024 / 1024);
-        println!("GPU Frequency: {} MHz", gpu_metrics.gpu_freq);
-        println!("================================================");
 
         let metrics_data = serialize::MetricsData {
             cpu_usage: cpu_ram_metrics.cpu_usage,
@@ -40,8 +26,12 @@ fn main() {
             gpu_freq: gpu_metrics.gpu_freq,
         };
         let serialized_data = serialize::serialize(&metrics_data).unwrap();
-            println!("Serialized Data: {:?}", serialized_data);
+        let port = match send::find_port() {
+            Some(port) => port,
+            None => continue,
+        };
+
+        println!("port: {}", &port)
     }
-
-
 }
+
---
+++ src/usb/send.rs
@@ -0,0 +1,15 @@
+use serialport;
+
+pub fn find_port() -> Option<String> {
+    let ports = serialport::available_ports().unwrap_or_else(|_| Vec::new());
+    for port in ports {
+        if let serialport::SerialPortType::UsbPort(info) = port.port_type {
+            if info.vid == 0x303A && info.pid == 0x3001 {
+                return Some(port.port_name);
+            }
+        }
+    }
+    eprintln!("ESP32 not found.");
+    None
+    
+}
